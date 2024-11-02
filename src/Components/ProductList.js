8eb12fa2fb036ec526cd9861122e85d3371a7405
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-11-02 23:01:35 +0300

    React eğitim

---
+++ src/Components/ProductList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+            Product Listesi
+        </h2>
+      </div>
+    )
+  }
+}
