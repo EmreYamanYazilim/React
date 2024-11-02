86b28bf4607ca5e731a2a30ae77f224e98305ca5
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-11-03 01:39:32 +0300

    state & döngü

--- src/Components/CategoryList.js
+++ src/Components/CategoryList.js
@@ -2,27 +2,41 @@ import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+    // constructor(props) {
+    //     super(props)
+
+    //     this.state = {
+    //         categories: [
+    //             { categoryId: 1, categoryName: "tatlı" },
+    //             { categoryId: 2, categoryName: "icecek" },
+    //             { categoryId: 3, categoryName: "yemek" },
+    //             { categoryId: 4, categoryName: "salata" }
+    //         ]
+    //     }
+    // }
+
+    // yukarıdaki gibide yapabiliriz direk sade şekilde alttaki gibide yapabiliriz yeni kullanım
+    state = {
+        categories: [
+            { categoryId: 1, categoryName: "tatlı" },
+            { categoryId: 2, categoryName: "icecek" },
+            { categoryId: 3, categoryName: "yemek" },
+            { categoryId: 4, categoryName: "salata" }
+        ]
+    }
+    
     render() {
         return (
             <div>
                 <h3>{this.props.info.categoryDeneme}</h3>
-                
+
                 <ListGroup>
-                    <ListGroupItem>
-                        Cras justo odio
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Dapibus ac facilisis in
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Morbi leo risus
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Porta ac consectetur ac
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Vestibulum at eros
-                    </ListGroupItem>
+                    {this.state.categories.map(category => (
+                        <ListGroupItem key={category.categoryId}>
+                            {category.categoryName}
+                        </ListGroupItem>
+                    ))}
+
                 </ListGroup>
             </div>
         )
